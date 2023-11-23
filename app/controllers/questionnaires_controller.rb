c643e03032af4cb544a962a0a48bdaf6867181fb
Hamed Aljamry <107309998+Hamed-Aljamry@users.noreply.github.com>
2023-11-23 17:33:58 +0000

    controller actions for questionnaire

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,7 +1,18 @@
 class QuestionnairesController < ApplicationController
   def new
+    @questionnaire = Questionnaire.new
+    setup_questions
   end
 
   def create
+  end
+
+  private
+  def setup_questions
+    @questions = [
+      { text: "What is your highest level of education?", options: ["High School", "Bachelor's", "Master's", "PhD"] },
+      { text: "What type of job are you looking for?", options: ["Full-time", "Part-time", "Internship", "Freelance"] },
+      # Add more questions as needed
+    ]
   end
 end
